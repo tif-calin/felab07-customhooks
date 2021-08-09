76c2096d611e20ebfd147f3512b30d560f18d75f
culi <culitif@tuta.io>
2021-08-09 02:42:19 -0700

    react router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import Home from '../home/Home';
+import List from '../list/List';
+import './App.scss';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Router><Switch>
+
+    <Route path="/" exact render={routerProps => (
+      <Home {...routerProps}/>
+    )}/>
+
+    <Route path="/list/:api" exact render={routerProps => (
+      <List {...routerProps}/>
+    )}/>
+
+    <Redirect to="/"/>
+
+  </Switch></Router>;
 }
