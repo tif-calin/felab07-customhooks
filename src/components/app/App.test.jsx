952293249d2620ab5e756cd8ce981598b528a365
culi <culitif@tuta.io>
2021-08-10 12:17:23 -0700

    snapshot and behavior tests

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -3,9 +3,12 @@ import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
+
   afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
+
+  test('renders App', () => {
+    const { asFragment } = render(<App/>);
     expect(asFragment()).toMatchSnapshot();
   });
+
 });
